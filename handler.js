0c2101726851995c1304f9c8352e5b921b657da9
Sergii <sergiigrytsaienko@gmail.com>
2019-01-24 22:10:45 -0600

    yml & handler updated

--- handler.js
+++ handler.js
@@ -1,14 +1,85 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.sendReminderDaily = (event, context, callback) => {
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    const AWS = require('aws-sdk');
+    AWS.config.update({region: 'us-east-1'});
+    const ses = new AWS.SES();
+    const fs = require('fs');
+
+    const emailHtml = fs.readFileSync('./dailyReminder.html', 'utf-8');
+
+    const toAndFromAdress = 'your_email_here'
+    const params = {
+        Destination: {
+            ToAddresses: [toAndFromAdress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: emailHtml
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: "Hi there, this is your daily reminder"
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Daily Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAdress],
+        Source: toAndFromAdress,
+    };
+
+    ses.sendEmail(params, function (err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack);
+        // successful response
+        else callback(null, data);
+    });
+};
+
+module.exports.sendReminderWeekend = (event, context, callback) => {
+
+    const AWS = require('aws-sdk');
+    AWS.config.update({region: 'us-east-1'});
+    const ses = new AWS.SES();
+    const fs = require('fs');
+
+    const emailHtml = fs.readFileSync('./weekendReminder.html', 'utf-8');
+
+    const toAndFromAdress = 'your_email_here\''
+    const params = {
+        Destination: {
+            ToAddresses: [toAndFromAdress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: emailHtml
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: "Hi there, this is your weekend reminder"
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Weekend Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAdress],
+        Source: toAndFromAdress,
+    };
+
+    ses.sendEmail(params, function (err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack);
+        // successful response
+        else callback(null, data);
+    });
 };
